4cc5bcb5cfabd9968f1f4dd7c83ae7123eac80e3
Jon Winsley <jon.winsley@gmail.com>
2020-10-21 22:46:53 -0400

    Adding test code as a starting point

--- src/CachedGameObject.spec.ts
+++ src/CachedGameObject.spec.ts
@@ -1,5 +1,19 @@
+import { CachedGameObject } from "CachedGameObject";
+import { mockGlobal, mockInstanceOf } from "../node_modules/screeps-jest/index";
+
 describe('CachedGameObject', () => {
+    beforeEach(() => {
+        mockGlobal<Memory>('Memory', {
+            cache: {}
+        }, true);
+        mockGlobal<Game>('Game', {
+            getObjectById: (id: Id<any>) => {
+                return mockInstanceOf<Structure>({id}, true);
+            }
+        })
+    })
     it('should pass', () => {
+        let o = new CachedGameObject<Structure>('struct' as Id<Structure>, ['id', 'pos']);
 
     })
 })
